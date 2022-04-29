44bd70d2446229d598853cd5522bf4767e427a49
Charles Vardeman <charles.vardeman@gmail.com>
2022-04-29 16:49:01 -0400

    Adding ontology build from modules

--- build.sh
+++ build.sh
@@ -2,28 +2,35 @@
 
 # WARNING: This is a quick hack that depends on the verison of python PDM installed
 pdmdir=`pdm info |grep Packages | cut -d: -f2`
-cmd="pdm run python ${pdmdir}/lib/rdfx/rdfx_cli.py"
+mergecmd="pdm run python ${pdmdir}/lib/rdfx/rdfx_cli.py"
+pylodecmd="pdm run pylode"
 files='./modules/common/metadata.ttl ./modules/common/system.ttl '
 shapefiles='./modules/common/metadata-shacl.ttl ./modules/common/system.shacl.ttl'
-ONTOLOGY=ammo.ttl
-SHACL=ammo.shacl.ttl
+ONTOLOGY=ammo
+SHACL=ammo.shacl
 
 VERSION=` grep -i versionInfo modules/common/metadata.ttl | sed 's/[^"]*"\([^"]*\).*/\1/'`
 
 if [ -f "$ONTOLOGY" ]; then
-    rm $ONTOLOGY
+    rm ${ONTOLOGY}.ttl
 fi
 
-$cmd merge $files -f ttl -o ./
-mv merged.ttl $ONTOLOGY
+if [ -f "$SHACL" ]; then
+    rm  ${SHACL}.ttl
+fi
 
-$cmd merge $shapefiles -f ttl -o ./
-mv merged.ttl $SHACL
+$mergecmd merge $files -f ttl -o ./
+mv merged.ttl ${ONTOLOGY}.ttl
+$pylodecmd ${ONTOLOGY}.ttl -o ${ONTOLOGY}.html
+
+$mergecmd merge $shapefiles -f ttl -o ./
+mv merged.ttl ${SHACL}.ttl
+# $pylodecmd $SHACL
 
 # Copy ontology file to a specific version
 if [ ! -d "./versions/${VERSION}" ]; then
-    mkdir ./verisons/${VERSION}
-    cp -f $ONTOLOGY $SHACL ./versions/$VERSION
+    mkdir ./versions/${VERSION}
+    cp -f ${ONTOLOGY}.* ${SHACL}.* ./versions/${VERSION}
 else
-    cp -f $ONTOLOGY $SHACL ./versions/$VERSION
+    cp -f ${ONTOLOGY}.* ${SHACL}.* ./versions/${VERSION}
 fi
